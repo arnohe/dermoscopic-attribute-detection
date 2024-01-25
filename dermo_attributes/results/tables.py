6943cba480cd2630dcece63a504b7777073149ec
Arno Heirman <43880877+arnohe@users.noreply.github.com>
2024-01-25 11:14:07 +0100

    Added scripts for processing results

---
+++ dermo_attributes/results/tables.py
@@ -0,0 +1,190 @@
+from itertools import product
+import pandas as pd
+import numpy as np
+import wandb
+import os
+
+from dermo_attributes.config import ATTRIBUTE_NAMES
+
+"""
+functions to tabulate and process the results
+"""
+
+
+def set_display_size():
+    """
+    increase size of printed tables
+    """
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+
+
+def get_best_runs(metric_name):
+    """
+    returns a table containing the best results for the metric given as argument
+    """
+    df = get_raw_results()
+    df = df[df["metric"] == metric_name]
+    best = [df[df["attribute"] == a].max()["id"] for a in ATTRIBUTE_NAMES]
+    return best
+
+
+def add_averages(df, metric_name="crisp_iou"):
+    """
+    adds average column to the table
+    """
+    metric = df.loc[:, (slice(None), metric_name)]
+    average = pd.DataFrame(metric.sum(axis=1) / 5)
+    average.columns = pd.MultiIndex.from_tuples([("average", metric_name)], names=["attritube", ""])
+    df = pd.concat([df, average], axis=1)
+    return df
+
+
+def table_all(df, metric_name="crisp_iou"):
+    """
+    returns the table containing an entry for the best in each subtype of loss function and overall best models
+    """
+    base_df = table_for_overview(df, metric_name)
+    best_df = table_for_best(df, metric_name)
+    rdf = pd.concat([best_df, base_df]).sort_index()
+    return rdf
+
+
+def table_for_overview(df, metric_name="crisp_iou"):
+    """
+    returns the table containing an entry for the best in each subtype of loss function
+    """
+    bce = table_for_selection(df, 0.5, 0, "binary focal loss", metric_name, "cross entropy")
+    dl = table_for_selection(df, 0.5, 1, "focal tversky loss", metric_name, "dice loss")
+    lcdl = table_for_selection(df, 0.5, -1, "log cosh tversky loss", metric_name, "log cosh dice loss")
+    wce = table_for_selection(df, None, 0, "binary focal loss", metric_name, "weighted cross entropy")
+    tl = table_for_selection(df, None, 1, "focal tversky loss", metric_name, "tversky loss")
+    ufl = table_for_selection(df, 0.5, None, "binary focal loss", metric_name, "unweighted focal loss")
+    fdl = table_for_selection(df, 0.5, None, "focal tversky loss", metric_name, "focal dice loss")
+
+    rdf = pd.concat([bce, dl, lcdl, wce, tl, ufl, fdl])
+    rdf = rdf.sort_index()
+    return rdf
+
+
+def table_for_best(df, metric_name="crisp_iou"):
+    """
+    returns the table containing the best results grouped by loss function and oversampling
+    """
+    metric = df.groupby(["balanced batches", "loss"]).max().stack()
+    idx = df.groupby(["balanced batches", "loss"]).idxmax()
+    alpha = idx.applymap(lambda k: k[-2]).stack()
+    gamma = idx.applymap(lambda k: k[-1] if k[-1] != -1 else np.nan).stack()
+
+    rdf = pd.concat([alpha, gamma, metric], axis=1)
+    rdf.columns = [["alpha", "gamma", metric_name]]
+    rdf = pd.pivot_table(rdf, index=["balanced batches", "loss"], columns=["attribute"],
+                         values=["alpha", "gamma", metric_name])
+    rdf = rdf.swaplevel(axis='columns').sort_index(axis=1, level=0)
+    return rdf
+
+
+def table_for_selection(df, alpha, gamma, loss_name, metric_name, new_loss_name=None):
+    """
+    returns the table containing best results for a subtype of loss function
+    """
+    if alpha is None:
+        alpha = slice(None)
+    if gamma is None:
+        gamma = slice(None)
+    if new_loss_name is None:
+        new_loss_name = loss_name
+
+    zdf = df.fillna(0, inplace=False)
+    metric = zdf.loc[(slice(None), loss_name, alpha, gamma), :].groupby(["balanced batches", "loss"]).max()
+    idx = zdf.loc[(slice(None), loss_name, alpha, gamma), :].groupby(["balanced batches", "loss"]).idxmax()
+
+    alpha = idx.applymap(lambda k: k[-2])
+    gamma = idx.applymap(lambda k: k[-1] if k[-1] != -1 else np.nan)
+    rdf = pd.concat([alpha, gamma, metric], axis=1, keys=["alpha", "gamma", metric_name])
+    rdf.columns = rdf.columns.droplevel(1)
+    rdf = rdf.swaplevel(axis='columns').sort_index(axis=1, level=0)
+    rdf.index = rdf.index.map(lambda k: (k[0], new_loss_name))
+    return rdf
+
+
+def get_raw_results():
+    """
+    reads raw results from experiments from disk (data/diagrams/raw_results_dataframe.pkl)
+    if not present retrieve them from wandb and save them to disk
+    """
+    if os.path.exists("data/results/raw_results_dataframe.pkl"):
+        return pd.read_pickle("data/results/raw_results_dataframe.pkl")
+
+    api = wandb.Api()
+    runs = api.runs("arno/lesion-attributes")
+    metrics = ["val_" + j + "_" + i for i, j in product(["iou", "recall", "precision"], ["fuzzy", "crisp", "class"])]
+    config = ["balanced_batches", "loss", "alpha", "gamma", "attribute_names"]
+
+    rows = []
+    for run in runs:
+        print(run.id)
+        if run.id == "1d5do82w":
+            best = 46
+        elif run.id == "jx62n5vf":
+            best = 25
+        else:
+            best = run.summary["best_epoch"]
+        run_results = run.history().loc[[best]].to_dict(orient="list")
+        row_conf = [run.config[conf] for conf in config]
+        row_conf[-1] = row_conf[-1][0]
+        for metr in metrics:
+            rows.append((run.id, *row_conf, metr[4:], float(run_results[metr][0])))
+    df = pd.DataFrame.from_records(data=rows, columns=["id", *config[:-1], "attribute", "metric", "value"])
+
+    df.to_pickle("data/results/raw_results_dataframe.pkl")
+    return df
+
+
+def get_multi_index_results(metric="crisp_iou"):
+    """
+    process raw results into a multi index table
+    """
+    df = get_raw_results()
+    df["loss"] = df["loss"].apply(
+        lambda k: k.replace("_", " "))
+    df["attribute"] = df["attribute"].apply(lambda k: k.replace("_", " "))
+    df.columns = df.columns.map(lambda k: k.replace("_", " "))
+    df = df.sort_values(by=["balanced batches", "loss", "attribute", "value"])
+    df = df[df["metric"] == metric]
+    df = pd.pivot_table(df, index=["balanced batches", "loss", "alpha", "gamma"], columns=["attribute"],
+                        values=["value"])
+    return df
+
+
+def print_formatted_table():
+    set_display_size()
+    df = add_averages(table_for_best(get_multi_index_results())).round(5)
+    loss_order = {"cross entropy": 0,
+                  "weighted cross entropy": 1,
+                  "binary focal loss": 3,
+                  "unweighted focal loss": 2,
+                  "dice loss": 4,
+                  "tversky loss": 5,
+                  "focal dice loss": 6,
+                  "focal tversky loss": 7,
+                  "log cosh dice loss": 8,
+                  "log cosh tversky loss": 9}
+    value_order = {"iou": 0,
+                   "alpha": 1,
+                   "gamma": 2}
+    class_order = {"globules": 0,
+                   "milia like cyst": 1,
+                   "negative network": 2,
+                   "pigment network": 3,
+                   "streaks": 4,
+                   "average": 5}
+
+    df = df.swaplevel()
+    df = df.sort_index(key=lambda x: x.map(loss_order))
+    df = df.transpose()
+    df = df.sort_index(level=1, key=lambda x: x.map(value_order))
+    df = df.sort_index(level=0, sort_remaining=False, key=lambda x: x.map(class_order))
+    df = df.transpose()
+    print(df)
---
+++ dermo_attributes/results/testing.py
@@ -0,0 +1,152 @@
+import os
+import cv2
+import wandb
+import pickle
+import numpy as np
+from itertools import repeat
+from tqdm.contrib.concurrent import process_map
+
+from dermo_attributes.io.paths import Splits
+from dermo_attributes.io.preprocess import read_crop_data
+from dermo_attributes.io.dataset import load_numpy_data, get_ids
+from dermo_attributes.io.images import threshold, get_isic_truth
+
+from dermo_attributes.config import ATTRIBUTE_NAMES
+from dermo_attributes.learning.training import load_model
+from dermo_attributes.results.figures import visualize_horizontal
+
+"""
+functions to make predictions on the test set
+"""
+
+
+def get_run_name_by_id(run_ids):
+    run_names = []
+    attributes = []
+    api = wandb.Api()
+    for run_id in run_ids:
+        run = api.run("arno/lesion-attributes/" + run_id)
+        attributes.append(*run.config["attributes"])
+        run_names.append(run.name)
+    return run_names, attributes
+
+
+def run_tests(run_ids):
+    """
+    calculate test scores for each attribute and mean
+    models given by their run id
+    """
+    run_names, attributes = get_run_name_by_id(run_ids)
+
+    id_list = get_ids("crop_512", Splits.TRAIN, [0])
+    dataset = load_numpy_data("crop_512", attributes, id_list, Splits.TRAIN)
+
+    predictions = []
+    for i, run_name in enumerate(run_names):
+        model = load_model(run_name)
+        predictions.append(model.predict(dataset[..., 0:3]))
+
+    predictions = np.array(predictions, dtype=np.float32)
+    predictions = predictions.squeeze(axis=4)
+    predictions = np.moveaxis(predictions, 0, 3)
+    crop_data = read_crop_data("crop_512")
+
+    pred_list = process_map(prepare_pred, predictions, [crop_data[str(idx)] for idx in id_list], chunksize=1)
+    gt_list = process_map(prepare_gt, id_list, repeat(attributes), chunksize=1)
+
+    pred_list = np.array(pred_list)
+    gt_list = np.array(gt_list)
+
+    jaccard = cumulative_jaccard(gt_list, pred_list)
+    return jaccard
+
+
+def prepare_pred(prediction, crop_data):
+    """
+    function to apply processing of prediction in parallel
+    """
+    return scale(uncrop(threshold(prediction), crop_data), size=512)
+
+
+def prepare_gt(isic_id, attributes):
+    """
+    function to apply processing of ground truth in parallel
+    """
+    return scale(get_isic_truth(isic_id, Splits.TRAIN, attributes), size=512)
+
+
+def cumulative_jaccard(gt, pred):
+    """
+    calculate cumulative jaccard metric for each of the attributes and mean
+    """
+    scores = []
+    for i in range(gt.shape[-1]):
+        tp = np.count_nonzero(np.logical_and(gt[..., i] == 1, pred[..., i] == 1))
+        fn = np.count_nonzero(np.logical_and(gt[..., i] == 1, pred[..., i] == 0))
+        fp = np.count_nonzero(np.logical_and(gt[..., i] == 0, pred[..., i] == 1))
+        scores.append(tp / (tp + fn + fp))
+    scores.append(n.array(scores).mean())
+    return scores
+
+
+def scale(masks, size):
+    """
+    function to rescale the predictions to shape: (size , size , M)
+    """
+    scaled = [cv2.resize(masks[:, :, j], (size, size), interpolation=cv2.INTER_NEAREST) for j in range(masks.shape[2])]
+    return np.stack(scaled, axis=2)
+
+
+def uncrop(pred, crop_data):
+    """
+    undoes the effects of preprocessing on the prediction
+    """
+    canvas = np.zeros((crop_data["y_shape"], crop_data["x_shape"], pred.shape[2]), dtype=pred.dtype)
+    up_scaled = scale(pred, crop_data["Z"])
+    yo1, yo2, xo1, xo2 = crop_data["yo1"], crop_data["yo2"], crop_data["xo1"], crop_data["xo2"]
+    yn1, yn2, xn1, xn2 = crop_data["yn1"], crop_data["yn2"], crop_data["xn1"], crop_data["xn2"]
+    canvas[yo1:yo2, xo1:xo2] = up_scaled[yn1:yn2, xn1:xn2]
+    return canvas
+
+
+def make_test_images(run_ids):
+    """
+    create a visualisation showing a prediction for each model
+    models are given by their run id
+    """
+    isic_examples = {0: 36236, 1: 36237, 2: 36291, 3: 15492, 4: 22039}
+
+    run_names, attributes = get_run_name_by_id(run_ids)
+
+    id_list = [isic_examples[a] for a in attributes]
+    dataset = load_numpy_data("crop_512", attributes, id_list, Splits.VALIDATION)
+
+    predictions = []
+    for i, run_name in enumerate(run_names):
+        predictions.append(load_model(run_name).predict(dataset[i:i + 1, :, :, 0:3]))
+
+    im = dataset[:, :, :, 0:3]
+    gt = np.array([dataset[i, :, :, 3 + k] for i, k in enumerate(attributes)])
+
+    predictions = np.array(predictions)
+    predictions = predictions.reshape([len(attributes), predictions.shape[-3], predictions.shape[-2]])
+
+    viz = visualize_horizontal(im, gt, predictions, [ATTRIBUTE_NAMES[k].replace("_", " ") for k in attributes])
+
+    return viz
+
+
+def make_test_predictions(run_name, dataset):
+    """
+    used to read and write predictions to disk
+    """
+    if os.path.exists("data/results/" + run_name + ".pkl"):
+        with open("data/results/" + run_name + ".pkl", "rb") as pickle_file:
+            return pickle.load(pickle_file)
+
+    model = load_model(run_name)
+    predictions = model.predict(dataset)
+    with open("data/results/" + run_name + ".pkl", 'wb') as pickle_file:
+        pickle.dump(predictions, pickle_file)
+
+    return predictions
